db3b6878635f8ddf90d475ee85b51588bd354c3d
Altan Şimşir <altansimsir@Altans-MacBook-Air.local>
2023-10-02 22:33:21 +0300

    no usage: getProducts method re-added

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -55,6 +55,10 @@ public class Basket {
         }
     }
 
+    public List<Product> getProducts() {
+        return products;
+    }
+
     public void setProducts(List<Product> products) {
         this.products = products;
     }
